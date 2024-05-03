7966bd1cde186409b8bc2813f0f22cb735141ccd
lavo <45769654+chucki1@users.noreply.github.com>
2024-05-03 15:13:42 +0200

    added method to clean ingredient to utils

--- utils.py
+++ utils.py
@@ -95,5 +95,47 @@ def clean_ingredients_list(df):
     df = pd.DataFrame(ingredients)
     df = df.map(lambda x: x if x is not None else '')
     df = df.map(clean_text)
+    nlp = stanza.Pipeline(lang='en', processors='tokenize, mwt, pos, lemma', use_gpu=True)
+   
+
+    ingredients_l = df.values.tolist()
+
+    cleaned_ingredients = []
+
+    with tqdm(total=len(df)) as pbar:
+        for row in ingredients_l:
+            cleaned_row = []
+            for ingredient in row:
+                # Process ingredient through the pipeline
+                doc = nlp(ingredient)
+                
+                # Extract tokenized forms, part-of-speech tags, and lemmatized forms
+                tokens = [word.text for sent in doc.sentences for word in sent.words]
+                pos_tags = [word.upos for sent in doc.sentences for word in sent.words]
+                lemmas = [word.lemma for sent in doc.sentences for word in sent.words]
+                
+                ### NOTICE THAT WE ARE USING `lemmas` INSTEAD OF `tokens`, so we will define our clean dictionary with the pure form of the words (their lemmatization!!!) ###
+                # eliminate the tokens in `tokens` that are ADJ in `pos_tags`
+                tokens = [lemmas[i] for i in range(len(tokens)) if pos_tags[i] != 'ADJ' and pos_tags[i] != 'PROPN']
+
+                # reconvert tokens to a string
+                cleaned_ingredient = ' '.join(tokens)
+
+                # append to the list
+                cleaned_row.append(cleaned_ingredient)
+
+            cleaned_ingredients.append(cleaned_row)
+            pbar.update(1)
+
+    # remove duplicates
+    for i in range(len(cleaned_ingredients)):
+        cleaned_ingredients[i] = list(set(cleaned_ingredients[i]))
+
+    # remove empty strings
+    cleaned_ingredients = [[x for x in row if x != ''] for row in cleaned_ingredients]
+
+    cleaned_df = pd.DataFrame(cleaned_ingredients)
+
+    return cleaned_df
 
 
